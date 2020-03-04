af640e8ec0cd9a293915ced591e710cf6b017776
Jake Daniel <jdaniel362@yahoo.com>
2020-03-03 21:42:50 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const characterAmountRange = document.getElementById("characterAmountRange")
+const characterAmountNumber = document.getElementById("characterAmountNumber")
+const form = document.getElementById("passwordGeneratorForm")
+const passwordDisplay = document.getElementById("passwordDisplay")
+const includeUpperCaseElement = document.getElementById("includeUpperCase")
+const includeSymbolsElement = document.getElementById("includeSymbols")
+const includeNumbersElement = document.getElementById("includeNumbers")
+
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126))
+
+characterAmountRange.addEventListener("input", syncCharacterAmount)
+characterAmountNumber.addEventListener("input", syncCharacterAmount)
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    const characterAmount = characterAmountNumber.value
+    const includeUpperCase = includeUpperCaseElement.checked
+    const includeSymbols = includeSymbolsElement.checked
+    const includeNumbers = includeNumbersElement.checked
+    console.log(characterAmount, includeUpperCase, includeSymbols, includeNumbers)
+    const password = generatePassword(characterAmount, includeUpperCase, includeNumbers, includeSymbols)
+    passwordDisplay.innerText = password
+})
+
+function generatePassword(characterAmount, includeUpperCase, includeNumbers, includeSymbols) {
+        let charCodes = LOWERCASE_CHAR_CODES
+        if (includeUpperCase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+        if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+        if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+        
+        const passwordCharacters = []
+        for (let i = 0; i < characterAmount; i++) {
+            const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+            passwordCharacters.push(String.fromCharCode(characterCode))
+        }
+        return passwordCharacters.join("")
+    }  
+
+function arrayFromLowToHigh(low, high) {
+        const array = []
+        for (let i = low; i <= high; i++) {
+            array.push(i)
+
+        }
+        return array
+    }
+//slider
+function syncCharacterAmount(e) {
+        const value = e.target.value
+        characterAmountNumber.value = value
+        characterAmountRange.value = value
+    }
